3fa8f9295d2de697d5d0830cf8174a685c61f8ab
Kipkorir2017 <pkobenj41@gmail.com>
2026-03-15 20:52:22 +0300

    add changes to controllers and Routes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,22 +1,24 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 module.exports = function(req,res,next){
 
-  const token = req.headers.authorization
+  const authHeader = req.headers.authorization;
 
-  if(!token)
-    return res.status(401).json({message:"No token"})
+  if(!authHeader)
+    return res.status(401).json({message:"No token"});
+
+  const token = authHeader.split(" ")[1];
 
   try{
 
-    const decoded = jwt.verify(token,"SECRETKEY")
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded
+    req.user = decoded;
 
-    next()
+    next();
 
   }catch(err){
 
-    res.status(401).json({message:"Invalid Token"})
+    res.status(401).json({message:"Invalid Token"});
   }
-}
+};
