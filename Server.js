69b25385098eb3f3286797670084f05159bc9cea
Kipkorir2017 <pkobenj41@gmail.com>
2026-03-20 20:39:43 +0300

    add changes to main file

--- Server.js
+++ Server.js
@@ -9,9 +9,12 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// 
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
 // Middleware
 app.use(cors());
-app.use(express.json()); // no need for body-parser (express already handles it)
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
