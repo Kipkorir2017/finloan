936494b9b1c415c6befeb90c0479aa5657c9a795
Kipkorir2017 <pkobenj41@gmail.com>
2026-03-22 19:37:00 +0300

    Fix connection to server

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const allowedOrigins = [
   "http://localhost:3000",
